fc90e75c294d996e997c02bb5129c2bb6a7b4000
Francesco Cioria <firefelix@gmail.com>
2018-08-24 14:44:09 +0200

    fix build: add babel-core dep and remove flexview css import

--- styleguide/setup.ts
+++ styleguide/setup.ts
@@ -1,16 +1,8 @@
-// import * as React from 'react';
-// import * as PropTypes from 'prop-types';
-
 import 'buildo-react-components/styleguide/setup';
 import 'react-autosize-textarea/styleguide/setup';
-import 'react-input-children/styleguide/setup';
 import 'react-cookie-banner/styleguide/setup';
 import 'react-flexview/styleguide/setup';
 
 import 'buildo-normalize-css';
-import 'react-flexview/src/flexView.scss';
 import './main.scss';
 import './codemirror.scss';
-
-// monkey patch React.PropTypes
-// React.PropTypes = PropTypes;
