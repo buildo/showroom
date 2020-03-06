4a4d3168f697c99df3341f73ca15163095df5781
aSlug <marpi93@icloud.com>
2020-03-06 17:16:30 +0100

    add quotes on import

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,42 +1,51 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 function brc(name) {
-  return path.resolve(__dirname, `node_modules/buildo-react-components/src/${name}/${name}.tsx`);
+  return path.resolve(
+    __dirname,
+    `node_modules/buildo-react-components/src/${name}/${name}.tsx`
+  );
 }
 
-const brcComponents = fs.readdirSync(path.resolve(__dirname, 'node_modules/buildo-react-components/src'))
-  .filter(c => [
-    'index.ts',
-    'utils',
-    'KitchenSink',
-    'InputChildren',
-    'Scroll',
-    'TransitionWrapper'
-  ].indexOf(c) === -1)
+const brcComponents = fs
+  .readdirSync(
+    path.resolve(__dirname, "node_modules/buildo-react-components/src")
+  )
+  .filter(
+    c =>
+      [
+        "index.ts",
+        "utils",
+        "KitchenSink",
+        "InputChildren",
+        "Scroll",
+        "TransitionWrapper"
+      ].indexOf(c) === -1
+  )
   .map(brc);
 
 module.exports = {
   // build
   serverPort: 8080,
-  styleguideDir: 'docs', // target of the `build` task
+  styleguideDir: "docs", // target of the `build` task
 
   require: [
     // "global" setup + sass imports
-    path.resolve(__dirname, 'styleguide/setup.ts')
+    path.resolve(__dirname, "styleguide/setup.ts")
   ],
   styleguideComponents: {
-    Logo: path.resolve(__dirname, 'styleguide/components/Logo.tsx'),
-    StyleGuide: path.join(__dirname, 'styleguide/components/StyleGuide.tsx')
+    Logo: path.resolve(__dirname, "styleguide/components/Logo.tsx"),
+    StyleGuide: path.join(__dirname, "styleguide/components/StyleGuide.tsx")
   },
 
   compilerConfig: {
-    objectAssign: 'Object.assign',
+    objectAssign: "Object.assign",
     transforms: { dangerousTaggedTemplateString: true }
   },
 
   // content
-  title: '@buildo/react-components',
+  title: "@buildo/react-components",
   template: {
     head: {
       raw: `
@@ -48,36 +57,70 @@ module.exports = {
       `
     }
   },
-  propsParser: require('react-docgen-typescript').parse, // detect docs using TS information
+  propsParser: require("react-docgen-typescript").parse, // detect docs using TS information
   sections: [
     {
-      name: 'Getting started',
-      content: 'sections/GettingStarted.md'
+      name: "Getting started",
+      content: "sections/GettingStarted.md"
     },
     {
-      name: 'Components',
-      components: brcComponents.concat([
-        path.resolve(__dirname, 'node_modules/react-autosize-textarea/src/TextareaAutosize.tsx'),
-        path.resolve(__dirname, 'node_modules/react-cookie-banner/src/CookieBanner.tsx'),
-        path.resolve(__dirname, 'node_modules/react-flexview/src/FlexView.tsx')
-      ]).sort((a, b) => a.split('/').slice(-1)[0].toLowerCase() > b.split('/').slice(-1)[0].toLowerCase() ? 1 : -1),
+      name: "Components",
+      components: brcComponents
+        .concat([
+          path.resolve(
+            __dirname,
+            "node_modules/react-autosize-textarea/src/TextareaAutosize.tsx"
+          ),
+          path.resolve(
+            __dirname,
+            "node_modules/react-cookie-banner/src/CookieBanner.tsx"
+          ),
+          path.resolve(
+            __dirname,
+            "node_modules/react-flexview/src/FlexView.tsx"
+          )
+        ])
+        .sort((a, b) =>
+          a
+            .split("/")
+            .slice(-1)[0]
+            .toLowerCase() >
+          b
+            .split("/")
+            .slice(-1)[0]
+            .toLowerCase()
+            ? 1
+            : -1
+        )
     }
   ],
   getComponentPathLine(componentPath) {
-    const name = path.basename(componentPath, '.tsx');
+    const name = path.basename(componentPath, ".tsx");
 
     switch (name) {
-      case 'TextareaAutosize': return `import TextareaAutosize from react-autosize-textarea`;
-      case 'CookieBanner': return `import CookieBanner from react-cookie-banner`;
-      case 'FlexView': return `import FlexView from 'react-flexview'`;
-      default: return `import ${name} from buildo-react-components/lib/${name}`;
+      case "TextareaAutosize":
+        return `import TextareaAutosize from 'react-autosize-textarea'`;
+      case "CookieBanner":
+        return `import CookieBanner from 'react-cookie-banner'`;
+      case "FlexView":
+        return `import FlexView from 'react-flexview'`;
+      default:
+        return `import ${name} from 'buildo-react-components/lib/${name}'`;
     }
   },
   getExampleFilename(componentPath) {
-    if (componentPath.includes('buildo-react-components')) {
-      return componentPath.split('/').slice(0, -1).concat('Examples.md').join('/');
+    if (componentPath.includes("buildo-react-components")) {
+      return componentPath
+        .split("/")
+        .slice(0, -1)
+        .concat("Examples.md")
+        .join("/");
     } else {
-      return componentPath.split('/').slice(0, -2).concat(['examples', 'Examples.md']).join('/');
+      return componentPath
+        .split("/")
+        .slice(0, -2)
+        .concat(["examples", "Examples.md"])
+        .join("/");
     }
   }
 };
